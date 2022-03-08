701fcadd0a7cce3ca700902a9fb704b74e04d52b
nilofarmujawar <99306976+nilofarmujawar@users.noreply.github.com>
2022-03-08 10:18:11 +0530

    Displays the balance based on the user input

--- src/com/workshop2/SavingAccountMain.java
+++ src/com/workshop2/SavingAccountMain.java
@@ -9,18 +9,18 @@ public class SavingAccountMain {
         SavingAccount saver1 = new SavingAccount(2000);
         SavingAccount saver2 = new SavingAccount(3000);
 
-        System.out.println("for saver 1");
+        System.out.println("...........for saver 1".........);
         SavingAccount.modifyInterestRate(4.0);
         saver1.calculateMonthlyInterest();
         saver1.displaySavings();
         saver1.calculateMonthlyInterest();
         saver1.displaySavings();
 
-        System.out.println("for saver 2");
+        System.out.println("...........for saver 2...........");
         SavingAccount.modifyInterestRate(5.0);
         saver2.calculateMonthlyInterest();
         saver2.displaySavings();
         saver2.calculateMonthlyInterest();
-        saver1.displaySavings();
+        saver2.displaySavings();
     }
 }
